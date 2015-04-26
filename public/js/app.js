9be8006ff620b1d8b6a175252caad567cee17acf
Chris Bacon <baconck@gmail.com>
2015-04-26 11:16:40 -0600

    add signup page

--- public/js/app.js
+++ public/js/app.js
@@ -156,7 +156,7 @@ function loadScript() {
   var script = document.createElement('script');
   script.type = 'text/javascript';
   script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
-      '&signed_in=true&callback=initialize&';
+      '&callback=initialize&';
   document.body.appendChild(script);
 }
 
