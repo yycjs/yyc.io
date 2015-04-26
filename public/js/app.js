c42ca18163f1e7a410f115226bcc1d168831245b
David Luecke <daff@neyeon.com>
2015-04-26 14:26:35 -0600

    Tweaking...

--- public/js/app.js
+++ public/js/app.js
@@ -179,6 +179,8 @@ $( "#slider" ).slider({
   step: 100,
   slide: function( event, ui ) {
     $( "#range" ).html( ui.value + 'm');
+    endpointParams.radius = ui.value;
+    drawPoints();
   }
 });
 $( "#range" ).html( $( "#slider" ).slider( "value" ) + 'm' );
@@ -192,3 +194,5 @@ $('#api-request').submit(function() {
   drawPoints(urlInput.val());
   return false;
 });
+
+setTimeout(drawPoints, 500);
